83049e1e1a1a202a2bb6c24214bf11f7454a57a4
Vince <vmr846@usask.ca>
2024-01-09 18:11:56 -0600

    added code for sound sensor program (program.py)

--- lab1/vmr846-lab01-part2/program.py
+++ lab1/vmr846-lab01-part2/program.py
@@ -1,2 +1,13 @@
-import RCF8591 as adc
+import PCF8591 as adc
 import RPi.GPIO
+
+GPIO.setmode(GPIO.BCM)
+
+adc.setup(0x48)
+
+count = 0
+while True:
+	val = adc.read(0)
+	if val:
+		print("Value: " + val)
+		time.sleep(0.2)
