78997991c72023f19e31b8c7c672c2c08cf86d7d
Vince <vmr846@usask.ca>
2024-01-26 15:01:57 -0600

    [LAB02] Changes to publisher.py in part 1

--- lab2/vmr846-lab02-part1/publisher.py
+++ lab2/vmr846-lab02-part1/publisher.py
@@ -8,7 +8,7 @@ client.connect(broker)
 
 while True:
     randNum = uniform(20.0,21.0)
-    client.publish("DB_LEVEL", randNum, 2, True) # To change the QoS level, change the argument "2" to your desired QoS level. To set/clear the retain flag, change the "True" argument
+    client.publish("DB_LEVEL", randNum, qos=0, retain=True) # To change the QoS level, change the argument "2" to your desired QoS level. To set/clear the retain flag, change the "True" argument
     print("Just published %s to topic \"DB_LEVEL\"" % str(randNum))
     time.sleep(1)
     
