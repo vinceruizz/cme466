b85ddfa116b0b0450da701fb201310974d040a43
Vince <vmr846@usask.ca>
2024-01-25 18:28:15 -0600

    [LAB02] Completed latency calculation system in subscriber.py and publisher.py

--- lab2/vmr846-lab02-part2/publisher.py
+++ lab2/vmr846-lab02-part2/publisher.py
@@ -1,7 +1,7 @@
 import paho.mqtt.client as mqtt
 import time
 from random import randrange, uniform
-import ntp
+import ntplib
 import json
 
 broker = "0.0.0.0"
@@ -9,10 +9,12 @@ client = mqtt.Client("client1")
 client.connect(broker)
 
 def getTime():
+	ntpClient = ntplib.NTPClient()
 	try:
-		result = ntp.time("pool.ntp.org")
-		print(f"Initial time: {result}")
-		return result
+		result = ntpClient.request("pool.ntp.org")
+		time = result.tx_time
+		print(f"Initial time: {time}")
+		return time
 	except Exception as e:
 		print(f"Error with NTP. Got: {e}")
 
@@ -22,6 +24,6 @@ while True:
 	initialTime = getTime()
 	msg = {"data":randNum,"timestamp":initialTime}
 	payload = json.dumps(msg)
-	client.publish("DB_LEVEL", payload, 2, True) # To change the QoS level, change the argument "2" to your desired QoS level. To set/clear the retain flag, change the "True" argument
+	client.publish("DB_LEVEL", payload) # To change the QoS level, change the argument "2" to your desired QoS level. To set/clear the retain flag, change the "True" argument
 	print("[PUBLISHER] Just published %s to topic \"DB_LEVEL\"" % json.loads(payload))
 	time.sleep(1) 
--- lab2/vmr846-lab02-part2/subscriber.py
+++ lab2/vmr846-lab02-part2/subscriber.py
@@ -1,8 +1,21 @@
 import paho.mqtt.client as mqtt
 import time
+import ntplib
+import json
 
 def on_message(client, userdata, message):
-	print("Received message: %s" % str(message.payload.decode("utf-8")))
+	received = json.loads(message.payload.decode("utf-8"))
+	data = received["data"]
+	initialTime = received["timestamp"]
+	print("Received message: %s" % str(data))
+
+	ntpClient = ntplib.NTPClient()
+	try:
+		currentTime = (ntpClient.request("pool.ntp.org")).tx_time
+		latency = currentTime - initialTime
+		print(f'Transmission latency: {latency}')
+	except Exception as e:
+		print(f'Error with NTP. Got: {e}')
 
 broker = "0.0.0.0"
 client = mqtt.Client("end_device")
