8ee2a5431995693d81afbaebf213669d67dfb75f
Vince Ruiz <vmr846@usask.ca>
2024-02-06 17:31:24 -0600

    [LAB03] Changes to msg_board.py

--- lab3/msg_board.py
+++ lab3/msg_board.py
@@ -1,5 +1,6 @@
 import paho.mqtt.client as mqtt
 import time
+import threading
 
 def on_message(client, userdata, message):
 	data = str(message.payload.decode("utf-8"))
@@ -9,11 +10,16 @@ broker = "broker.hivemq.com"
 client = mqtt.Client("msg_board_client_ruiz")
 client.connect(broker)
 
-client.loop_forever()
-client.subscribe("MSG_BOARD") # you can change the QoS by adding parameter qos=x (replace x with desired QoS level (0, 1, 2)
-client.on_message = on_message
+def receive():
+    client.loop_forever()
+    client.subscribe("MSG_BOARD") # you can change the QoS by adding parameter qos=x (replace x with desired QoS level (0, 1, 2)
+    client.on_message = on_message
 
-while True:
-	end_connection = input(">> ")
-	if end_connection == "end":
-		client.disconnect()
+def end_connection():
+    while True:
+        end_connection = input(">> ")
+        if end_connection == "end":
+            client.disconnect()
+        
+x1 = threading.Thread(target=receive)
+x2 = threading.Thread(target=end_connection)
