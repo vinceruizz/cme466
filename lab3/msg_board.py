e0628dbd16d85973646cdc0777284df016d3ba14
Vince <vmr846@usask.ca>
2024-02-06 17:38:47 -0600

    too lazy

--- lab3/msg_board.py
+++ lab3/msg_board.py
@@ -10,16 +10,17 @@ broker = "broker.hivemq.com"
 client = mqtt.Client("msg_board_client_ruiz")
 client.connect(broker)
 
-def receive():
-    client.loop_forever()
-    client.subscribe("MSG_BOARD") # you can change the QoS by adding parameter qos=x (replace x with desired QoS level (0, 1, 2)
-    client.on_message = on_message
+
+client.loop_start()
+client.subscribe("MSG_BOARD") # you can change the QoS by adding parameter qos=x (replace x with desired QoS level (0, 1, 2)
+client.on_message = on_message
 
 def end_connection():
     while True:
         end_connection = input(">> ")
         if end_connection == "end":
-            client.disconnect()
-        
-x1 = threading.Thread(target=receive)
+            client.loop_stop()
+			print("connection to broker ended")
+
 x2 = threading.Thread(target=end_connection)
+x2.start()
